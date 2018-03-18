01f313bfdca2e4e16f68e4a0d9b69e82b2d81c6e
jjreed05 <jjreed05@gmail.com>
2018-03-17 22:00:24 -0600

    done

--- node-js-getting-started/model.js
+++ node-js-getting-started/model.js
@@ -1,10 +1,8 @@
 const pg = require('pg')
-const config = 'postgres://qvoldvoweajcmz:d48c38e38adaea162e827eb72f4f244cbfd3f0cf57e07cb3eb5808671d873a79@ec2-54-163-246-193.compute-1.amazonaws.com:5432/deq1s8sdg5mfhj'
-const pool = new pg.Pool(config)
 
 function getUser(req, res, next){
     console.log('Getting Credentials')
-    pool.connect(function (err, client, done){
+    pg.connect(process.env.DATABASE_URL, function (err, client, done){
         if (err) {
             return console.log("Error fetching from pool")
         }
@@ -22,7 +20,7 @@ function getUser(req, res, next){
 
 function getEntry(req, res, next){
     console.log('Getting Credentials')
-    pool.connect(function (err, client, done){
+    pg.connect(process.env.DATABASE_URL, function (err, client, done){
         if (err) {
             return console.log("Error fetching from pool")
         }
