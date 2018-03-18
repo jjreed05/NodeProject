218b1d5e6308eef7b65994c0baea4ca5392a9f52
jjreed05 <jjreed05@gmail.com>
2018-03-17 21:54:26 -0600

    initializing

--- node-js-getting-started/model.js
+++ node-js-getting-started/model.js
@@ -2,7 +2,6 @@ const pg = require('pg')
 const config = 'postgres://qvoldvoweajcmz:d48c38e38adaea162e827eb72f4f244cbfd3f0cf57e07cb3eb5808671d873a79@ec2-54-163-246-193.compute-1.amazonaws.com:5432/deq1s8sdg5mfhj'
 const pool = new pg.Pool(config)
 
-const pool = new pg.Pool(connectionString)
 function getUser(req, res, next){
     console.log('Getting Credentials')
     pool.connect(function (err, client, done){
