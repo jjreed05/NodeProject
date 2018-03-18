5597c543230180f5e9f6f0bb2c92e73b902769df
jjreed05 <jjreed05@gmail.com>
2018-03-17 22:13:44 -0600

    fixed bug

--- node-js-getting-started/model.js
+++ node-js-getting-started/model.js
@@ -1,11 +1,10 @@
 const pg = require('pg')
-
-const connectionString = 'postgres://qvoldvoweajcmz:d48c38e38adaea162e827eb72f4f244cbfd3f0cf57e07cb3eb5808671d873a79@ec2-54-163-246-193.compute-1.amazonaws.com:5432/deq1s8sdg5mfhj
-'
+const config = 'postgres://qvoldvoweajcmz:d48c38e38adaea162e827eb72f4f244cbfd3f0cf57e07cb3eb5808671d873a79@ec2-54-163-246-193.compute-1.amazonaws.com:5432/deq1s8sdg5mfhj'
+const pool = new pg.Pool(config)
 
 function getUser(req, res, next){
     console.log('Getting Credentials')
-    pg.connect(connectionString, function (err, client, done){
+    pool.connect(function (err, client, done){
         if (err) {
             return console.log("Error fetching from pool")
         }
@@ -23,7 +22,7 @@ function getUser(req, res, next){
 
 function getEntry(req, res, next){
     console.log('Getting Credentials')
-    pg.connect(connectionString, function (err, client, done){
+    pool.connect(function (err, client, done){
         if (err) {
             return console.log("Error fetching from pool")
         }
